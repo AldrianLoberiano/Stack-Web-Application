2fd5c9596d40a1435f77d47c858071f0ea19a428
AldrianLoberiano <loberianorian@gmail.com>
2026-04-29 21:04:41 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -32,6 +32,7 @@ const PORT = process.env.PORT || 5000;
 
 const startServer = async () => {
   try {
+    await initializeDatabase();
     app.listen(PORT, () => {
       console.log(`🚀  Server running on http://localhost:${PORT}`);
     });
