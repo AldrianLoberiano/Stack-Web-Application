607839dbd4ef34d35227fbad6262c24344997648
AldrianLoberiano <loberianorian@gmail.com>
2026-04-29 21:04:43 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -33,6 +33,7 @@ const PORT = process.env.PORT || 5000;
 const startServer = async () => {
   try {
     await initializeDatabase();
+    console.log('✅  SQLite database ready');
     app.listen(PORT, () => {
       console.log(`🚀  Server running on http://localhost:${PORT}`);
     });
