c93ae5d6c54d3cb5bd618d365ff20011c4739391
AldrianLoberiano <loberianorian@gmail.com>
2026-04-29 21:04:50 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -38,7 +38,6 @@ const startServer = async () => {
     app.listen(PORT, () => {
       console.log(`🚀  Server running on http://localhost:${PORT}`);
     });
-  .catch((err) => {
     console.error('❌  MongoDB connection error:', err.message);
     process.exit(1);
   });
