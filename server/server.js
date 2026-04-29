ff15cb203107d3a74c9642fa270e0120c30d335b
AldrianLoberiano <loberianorian@gmail.com>
2026-04-29 21:04:22 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -30,7 +30,6 @@ app.use((_req, res) => {
 // ─── Database & Server ───────────────────────────────────────────────────────
 const PORT = process.env.PORT || 5000;
 
-    console.log('✅  MongoDB connected');
     app.listen(PORT, () => {
       console.log(`🚀  Server running on http://localhost:${PORT}`);
     });
