f4ebac076cbdf30da93d5e154db0083de43c7520
AldrianLoberiano <loberianorian@gmail.com>
2026-04-29 21:02:37 +0800

    Update userController.js

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -123,7 +123,6 @@ const getMe = async (req, res) => {
     }
     res.status(200).json({
       success: true,
-        lastName: user.lastName,
         email: user.email,
         company: user.company,
         plan: user.plan,
