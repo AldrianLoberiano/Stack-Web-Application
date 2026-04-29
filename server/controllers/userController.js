e355a03b17fd6b9bae08d9b8eceff3ab5af175b8
AldrianLoberiano <loberianorian@gmail.com>
2026-04-29 21:00:33 +0800

    Update userController.js

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -45,7 +45,6 @@ const registerUser = async (req, res) => {
       });
     }
 
-    const existing = await User.findOne({ email });
     if (existing) {
       return res.status(409).json({
         success: false,
