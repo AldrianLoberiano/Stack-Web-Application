05bba2b5f187c40fe26c1def2a6d1feaa48e2a7a
AldrianLoberiano <loberianorian@gmail.com>
2026-04-30 23:36:19 +0800

    Update sqlite.js

--- server/db/sqlite.js
+++ server/db/sqlite.js
@@ -9,3 +9,4 @@ const initializeDatabase = async () => {
 
 const createUser = async ({ firstName, lastName, email, password, company, plan }) => {
   const now = new Date().toISOString();
+  const user = {
