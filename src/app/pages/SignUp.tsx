766c81f11499d5164e0e5539e9ec7ac1d5fe51b2
AldrianLoberiano <loberianorian@gmail.com>
2026-05-01 21:13:08 +0800

    Update SignUp.tsx

--- src/app/pages/SignUp.tsx
+++ src/app/pages/SignUp.tsx
@@ -314,6 +314,7 @@ export function SignUp() {
                     ) : (
                       <span style={{ display: "inline-flex", alignItems: "center", gap: "8px" }}>
                         <PartyPopper size={16} />
+                      </span>
                   </button>
                 </div>
               </div>
